15c2a843d6dd67f6d3dd49e087797f79046b22ef
Kendall <ktrudickm@gmail.com>
2021-03-02 17:18:09 -0500

    Added function for handling the search button to capture user input. Created list of search history, and added event listener to clear history

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,10 @@
 var searchBtn = document.getElementById('searchbtn');
+var searchHistoryEl = document.getElementById('search-history');
+var clearBtn = document.getElementById('clear-history');
 
 
+
+// Function to handle saving user input value for the city name and saving it to local storage
 function handleSearchFormSubmit(event) {
     event.preventDefault();
   
@@ -11,23 +15,44 @@ function handleSearchFormSubmit(event) {
       return;
     }
 
-  
-    localStorage.setItem('city')
-    getApi;
+    console.log(searchInputVal);
+    localStorage.setItem('city', searchInputVal);
+    displayHistory(searchInputVal);
+    getApi(searchInputVal);
 }
 
-
+// Function to get API based on user's city name input
 function getApi() {
-
+    var savedCity = localStorage.getItem('city');
+    var weatherApi = "https://api.openweathermap.org/data/2.5/forecast?q=" + savedCity + "&appid=41f0b59a1e00c5b966c2d7bde52a04f5&units=imperial";
+    console.log(weatherApi);
 
 }
 
 
+// Function to display search history
+function displayHistory(){
+    var history = document.createElement('ul');
+    searchHistoryEl.append(history);
+    var cityName = document.createElement('li');
+    var cityListText = document.createTextNode(localStorage.getItem('city'));
+
+    cityName.append(cityListText);
+    history.appendChild(cityName);
+    console.log(history);
+
+    cityName.classList.add("listItm");
+    cityName.style.listStyle = "none";
+}
+
+// Event Listener for clearing city name search history
+clearBtn.addEventListener('click', function clearHistory() {
+    searchHistoryEl.innerHTML = "";
+    localStorage.clear();
+});
 
 
-
-
-
-
-
+// Event listener for when user clicks search button after inputting a city name
 searchBtn.addEventListener('click', handleSearchFormSubmit);
+
+
