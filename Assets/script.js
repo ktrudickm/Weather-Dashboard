798160b485e16a72a733420dcfdfa4a12993f2da
Kendall <ktrudickm@gmail.com>
2021-03-05 17:16:28 -0500

    Added comments to script file

--- Assets/script.js
+++ Assets/script.js
@@ -19,7 +19,6 @@ function handleSearchFormSubmit(event) {
     event.preventDefault();
   
     searchInputVal = document.querySelector('#searchinput').value;
-    console.log(historyArr)
     if(historyArr.indexOf(searchInputVal) === -1){
         historyArr.push(searchInputVal);
         localStorage.setItem('history', JSON.stringify(historyArr));
@@ -29,8 +28,6 @@ function handleSearchFormSubmit(event) {
       console.error('You need a search input value!');
       return;
     }
-
-    console.log(searchInputVal);
    
     getApi(searchInputVal);
 }
@@ -40,20 +37,20 @@ function getApi(city) {
     
     var weatherApi = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=41f0b59a1e00c5b966c2d7bde52a04f5&units=imperial";
     var currentApi = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=41f0b59a1e00c5b966c2d7bde52a04f5&units=imperial";
-    console.log(weatherApi);
 
     cityEl.innerHTML = "";
     fiveDay.innerHTML = "";
     tempEl.innerHTML = "";
     humidityEl.innerHTML = "";
     windEl.innerHTML = "";
+    uvEL.innerHTML = "";
+    uvVal.innerHTML = "";
 
     fetch(weatherApi)
         .then(function (response) {
             if (!response.ok) {
                 throw response.json();
             }
-            console.log(response);
             return response.json();
         })
         // Handles the five day data and loops through the list array and assigns to variables in order to display onto page.
@@ -102,13 +99,10 @@ function getApi(city) {
 }
 
 
-
+// Function to display the current weather information
 function displayCurrent(currentData){
 
-    console.log('This is CURRENT from current function');
-    console.log(currentData);
-    console.log(currentData.main.temp);
-
+    //Need to get the lat and lon of city in order to get the UV Index 
     var Lat = currentData.coord.lat;
     var Lon = currentData.coord.lon;
     getUV(Lat, Lon);
@@ -135,6 +129,7 @@ function displayCurrent(currentData){
 
 }
 
+// Function to get what the UV index is for the city entered
 function getUV(latitude, longitude) {
     var uvAPI = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=41f0b59a1e00c5b966c2d7bde52a04f5";
 
@@ -147,14 +142,13 @@ function getUV(latitude, longitude) {
             return response.json();
         })
         .then(function(data) {
-            console.log(data);
-            console.log("THIS IS UV DATA")
             uvData = data.value;
             uvStyle(uvData);
             
         })
 }
 
+// Function to handle giving the UV Index value a background/highlight color based on its value.
 function uvStyle(uvData){
     uvVal.append(document.createTextNode(uvData));
 
@@ -177,10 +171,8 @@ function displayHistory(city){
 
     var cityName = document.createElement('li');
     cityName.textContent = city;
-    console.log(cityName,history)
 
     cityName.onclick = function (){
-        console.log("i was clicked", this.textContent)
         getApi(this.textContent)
     }
     
@@ -196,7 +188,6 @@ var historyArr = JSON.parse(localStorage.getItem('history')) || [];
 
 if(historyArr.length){
     for (let i = 0; i < historyArr.length; i++) {
-        console.log(historyArr[i])
        displayHistory(historyArr[i])
         
     }
